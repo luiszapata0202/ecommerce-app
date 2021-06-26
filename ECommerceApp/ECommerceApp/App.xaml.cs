d53e67bef054d45063de07f1a960a0d66e71f09f
Luis David Lopez Zapata <luisdavidlz0202@gmail.com>
2021-06-26 09:39:07 -0500

    1. SignUpPage created

---
+++ ECommerceApp/ECommerceApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ECommerceApp.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ECommerceApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SignUpPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ECommerceApp/ECommerceApp/Pages/SignUpPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ECommerceApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUpPage : ContentPage
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();            
+        }
+
+        private void btnContinue_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
