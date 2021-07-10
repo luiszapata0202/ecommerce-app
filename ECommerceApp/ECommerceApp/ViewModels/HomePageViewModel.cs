7fb4d4b437d16db751c65579f245431760e59ab9
Luis David Lopez Zapata <luisdavid.lopez@globant.com>
2021-07-10 13:00:47 -0500

    Flyout Menu implemented

--- ECommerceApp/ECommerceApp/ViewModels/HomePageViewModel.cs
+++ ECommerceApp/ECommerceApp/ViewModels/HomePageViewModel.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
 using ECommerceApp.Interfaces;
 using ECommerceApp.Models;
+using Prism.Commands;
 using Prism.Navigation;
+using Xamarin.Essentials;
 
 namespace ECommerceApp.ViewModels
 {
@@ -10,6 +15,14 @@ namespace ECommerceApp.ViewModels
         #region Private Attributes
         private readonly IProductService _productService;
         private readonly IShoppingCartService _shoppingCartService;
+        private int _shoppingCartItemsCount;
+
+        public int ShoppingCartItemsCount
+        {
+            get { return _shoppingCartItemsCount; }
+            set { SetProperty(ref _shoppingCartItemsCount, value); }
+        }
+
         #endregion
 
         #region Constructor
@@ -23,10 +36,16 @@ namespace ECommerceApp.ViewModels
 
             PopularProducts = new ObservableCollection<Product>();
             Categories = new ObservableCollection<Category>();
+            ShowMenuCommand = new DelegateCommand(ShowMenu);
+            ShoppingCartCommand = new DelegateCommand(ShoppingCart);
+            LogoutCommand = new DelegateCommand(async () => await Logout());
         }
         #endregion
 
         #region Public Properties
+        public DelegateCommand ShowMenuCommand { get; set; }
+        public DelegateCommand ShoppingCartCommand { get; set; }
+        public DelegateCommand LogoutCommand { get; set; }
         public ObservableCollection<Product> PopularProducts { get; set; }
         public ObservableCollection<Category> Categories { get; set; }
         #endregion
@@ -47,6 +66,30 @@ namespace ECommerceApp.ViewModels
             }
 
             var cartItemsCount = await _shoppingCartService.GetCartItemsCount();
+            ShoppingCartItemsCount = cartItemsCount;
+        }
+
+        private void ShowMenu()
+        {
+           
+        }
+
+        private void ShoppingCart()
+        {
+           
+        }
+
+        private async Task Logout()
+        {
+            var response = await UserDialogs.Instance.ConfirmAsync("Are you sure you want to logout?", "Logout", "Yes", "Cancel");
+
+            if (response)
+            {
+                Preferences.Set("accessToken", string.Empty);
+                Preferences.Set("tokenExpirationTime", string.Empty);
+
+                await NavigationService.NavigateAsync(new Uri("app:///NavigationPage/SignUpPage"));
+            }
         }
         #endregion
     }
